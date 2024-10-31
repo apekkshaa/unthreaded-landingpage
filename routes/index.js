4af44acb6143602f76db3482050a04f994602b04
Apeksha Rajput <apeksharajput@Apekshas-MacBook-Air.local>
2024-10-31 14:14:03 +0530

    routes connected

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./users');
+const path = require('path');
 
-// Example route to add a user
-router.post('/addUser', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const newUser = new User({ name, email, password });
-    await newUser.save();
-    res.send('User added successfully');
-  } catch (err) {
-    console.log('Error:', err);
-    res.status(500).send('Server error');
-  }
+//landing page route
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../html/index.html'));
 });
 
+// Signup page route
+router.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '../html/signup.html'));
+});
+
+//login route
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../html/login.html'));
+});
 module.exports = router;
